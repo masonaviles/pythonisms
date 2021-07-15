5cac7ef2ba2aecfedea15474f3828428a7809914
Mason Aviles <mce.aviles@gmail.com>
2021-07-15 01:11:29 -0700

    add some data structures

--- linkedlist.py
+++ linkedlist.py
@@ -1,147 +1,73 @@
-class node:
-	def __init__(self,data=None):
-		self.data=data
-		self.next=None
+import random
 
-class linked_list_iterator:
-    def __init__(self, head):
-        self.current = head
+
+class LinkedListNode:
+    def __init__(self, value, next_node=None, prev_node=None):
+        self.value = value
+        self.next = next_node
+        self.prev = prev_node
+
+    def __str__(self):
+        return str(self.value)
+
+
+class LinkedList:
+    def __init__(self, values=None):
+        self.head = None
+        self.tail = None
+        if values is not None:
+            self.add_multiple(values)
 
     def __iter__(self):
-        return self
+        current = self.head
+        while current:
+            yield current
+            current = current.next
 
-    def __next__(self):
-        if not self.current:
-            raise StopIteration
+    def __str__(self):
+        values = [str(x) for x in self]
+        return " -> ".join(values)
+
+    def __len__(self):
+        result = 0
+        node = self.head
+        while node:
+            result += 1
+            node = node.next
+        return result
+
+    def values(self):
+        return [x.value for x in self]
+
+    def add(self, value):
+        if self.head is None:
+            self.tail = self.head = LinkedListNode(value)
         else:
-            item = self.current.get_data()
-            self.current = self.current.get_next()
-            return item
+            self.tail.next = LinkedListNode(value)
+            self.tail = self.tail.next
+        return self.tail
 
-class linked_list:
-	def __init__(self):
-		self.head=node()
-	
-	def __iter__(self):
-    	return linked_list_iterator(self.head)
+    def add_to_beginning(self, value):
+        if self.head is None:
+            self.tail = self.head = LinkedListNode(value)
+        else:
+            self.head = LinkedListNode(value, self.head)
+        return self.head
 
-	# Adds new node containing 'data' to the end of the linked list.
-	def append(self,data):
-		new_node=node(data)
-		cur=self.head
-		while cur.next!=None:
-			cur=cur.next
-		cur.next=new_node
+    def add_multiple(self, values):
+        for v in values:
+            self.add(v)
 
-	# Returns the length (integer) of the linked list.
-	def length(self):
-		cur=self.head
-		total=0
-		while cur.next!=None:
-			total+=1
-			cur=cur.next
-		return total 
-
-	# Prints out the linked list in traditional Python list format. 
-	def display(self):
-		elems=[]
-		cur_node=self.head
-		while cur_node.next!=None:
-			cur_node=cur_node.next
-			elems.append(cur_node.data)
-		print(elems)
-
-	# Returns the value of the node at 'index'. 
-	def get(self,index):
-		if index>=self.length() or index<0: # added 'index<0' post-video
-			print("ERROR: 'Get' Index out of range!")
-			return None
-		cur_idx=0
-		cur_node=self.head
-		while True:
-			cur_node=cur_node.next
-			if cur_idx==index: return cur_node.data
-			cur_idx+=1
-
-	# Deletes the node at index 'index'.
-	def erase(self,index):
-		if index>=self.length() or index<0: # added 'index<0' post-video
-			print("ERROR: 'Erase' Index out of range!")
-			return 
-		cur_idx=0
-		cur_node=self.head
-		while True:
-			last_node=cur_node
-			cur_node=cur_node.next
-			if cur_idx==index:
-				last_node.next=cur_node.next
-				return
-			cur_idx+=1
-
-	# Allows for bracket operator syntax (i.e. a[0] to return first item).
-	def __getitem__(self,index):
-		return self.get(index)
+    @classmethod
+    def generate(cls, k, min_value, max_value):
+        return cls(random.choices(range(min_value, max_value), k=k))
 
 
-	#######################################################
-	# Functions added after video tutorial
-
-	# Inserts a new node at index 'index' containing data 'data'.
-	# Indices begin at 0. If the provided index is greater than or 
-	# equal to the length of the linked list the 'data' will be appended.
-	def insert(self,index,data):
-		if index>=self.length() or index<0:
-			return self.append(data)
-		cur_node=self.head
-		prior_node=self.head
-		cur_idx=0
-		while True:
-			cur_node=cur_node.next
-			if cur_idx==index: 
-				new_node=node(data)
-				prior_node.next=new_node
-				new_node.next=cur_node
-				return
-			prior_node=cur_node
-			cur_idx+=1
-
-	# Inserts the node 'node' at index 'index'. Indices begin at 0.
-	# If the 'index' is greater than or equal to the length of the linked 
-	# list the 'node' will be appended.
-	def insert_node(self,index,node):
-		if index<0:
-			print("ERROR: 'Erase' Index cannot be negative!")
-			return
-		if index>=self.length(): # append the node
-			cur_node=self.head
-			while cur_node.next!=None:
-				cur_node=cur_node.next
-			cur_node.next=node
-			return
-		cur_node=self.head
-		prior_node=self.head
-		cur_idx=0
-		while True:
-			cur_node=cur_node.next
-			if cur_idx==index: 
-				prior_node.next=node
-				return
-			prior_node=cur_node
-			cur_idx+=1
-
-	# Sets the data at index 'index' equal to 'data'.
-	# Indices begin at 0. If the 'index' is greater than or equal 
-	# to the length of the linked list a warning will be printed 
-	# to the user.
-	def set(self,index,data):
-		if index>=self.length() or index<0:
-			print("ERROR: 'Set' Index out of range!")
-			return
-		cur_node=self.head
-		cur_idx=0
-		while True:
-			cur_node=cur_node.next
-			if cur_idx==index: 
-				cur_node.data=data
-				return
-			cur_idx+=1
+class DoublyLinkedList(LinkedList):
+    def add(self, value):
+        if self.head is None:
+            self.tail = self.head = LinkedListNode(value)
+        else:
+            self.tail.next = LinkedListNode(value, None, self.tail)
+            self.tail = self.tail.next
+        return self
